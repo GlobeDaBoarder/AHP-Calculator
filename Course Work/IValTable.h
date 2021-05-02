6218a25d8acde2b4d85f249ecfa8f6cfcec1252b
Gleb Ivashyn <gleb.ivashyn@stud.vgtu.lt>
2021-05-02 17:27:05 +0300

    Adding value calculation classes

---
+++ Course Work/IValTable.h
@@ -0,0 +1,6 @@
+#pragma once
+class IValTable
+{
+public:
+
+};
---
+++ Course Work/ValTableBase.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <vector>
+class ValTableBase
+{
+private:
+	std::vector<std::vector<double>> m_content;
+
+public:
+
+
+};

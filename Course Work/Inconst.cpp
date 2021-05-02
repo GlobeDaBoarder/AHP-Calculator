a0edafcc25dd83edbe27620c2d131d5d6f98a684
Gleb Ivashyn <gleb.ivashyn@stud.vgtu.lt>
2021-05-02 16:26:46 +0300

    adding inconsistency calculations

--- Course Work/Inconst.cpp
+++ Course Work/Inconst.cpp
@@ -1,6 +1,20 @@
 #include "Inconst.h"
+#include <iomanip>
 
 //private
+
+const double Inconst::k_randomIndex = 1.12;
+
+double Inconst::lambdaAvg()
+{
+	double tempAvg = 0;
+	for (double& el : m_lambda)
+	{
+		tempAvg += el;
+	}
+
+	return tempAvg / m_lambda.size();
+}
 
 void Inconst::printweights()
 {
@@ -10,6 +24,33 @@ void Inconst::printweights()
 		printInFormat(i, '%');
 	}
 	printLine();
+}
+
+void Inconst::printWeightSum()
+{
+	printInFormat("WEIGHTED SUM");
+	for (auto& i : m_weightsum)
+	{
+		printInFormat(i, ' ' , 4);
+	}
+	printLine();
+}
+
+void Inconst::printLambda()
+{
+	printInFormat("LAMBDA");
+	for (auto& i : m_lambda)
+	{
+		printInFormat(i, ' ', 6);
+	}
+	printLine();
+}
+
+void Inconst::printConsisCalc()
+{
+	std::cout << std::setw(20) << ' '  << "Lambda avg = " << m_lambdaAvg << std::endl;
+	std::cout << std::setw(20) << ' ' << "Consistency index = (Lambda avg - n)/n-1= " << m_consisIndex << std::endl;
+	std::cout << std::setw(20) << ' '  << "Consistency ratio " << m_consisratio * 100 << '%' << std::endl;
 }
 
 //public
@@ -22,13 +63,17 @@ Inconst::Inconst(PairwiseComp& PCTable, Stand& stand) : TableBase()
 		double tempSum = 0;
 		for (int j = 0; j < 5; ++j)
 		{
-			m_content[i][j] = PCTable.getContent()[i][j] * m_weightscopy[i]/100;
+			m_content[i][j] = PCTable.getContent()[i][j] * (m_weightscopy[j]/100);
 			tempSum += m_content[i][j];
 		}
 
 		m_weightsum.push_back(tempSum);
-		m_lambda.push_back(m_weightsum[i] / m_weightscopy[i]/100);
+		m_lambda.push_back(m_weightsum[i] /( m_weightscopy[i]/100));
 	}
+
+	m_lambdaAvg = lambdaAvg();
+	m_consisIndex = (m_lambdaAvg - m_lambda.size()) / (m_lambda.size() - 1);
+	m_consisratio = m_consisIndex / k_randomIndex;
 }
 
 Inconst::~Inconst()
@@ -37,12 +82,19 @@ Inconst::~Inconst()
 
 void Inconst::printTable(ChosenParam& chosenparams)
 {
-	std::cout << "\n\nConsistency matrix\n";
+	std::cout << "\n\nInonsistency matrix(every element of pairwise comparison multiplied by the weight o it's row\n";
 
 	printweights();
 	TableBase::printTable(chosenparams);
 
-	std::cout << "Weight represents average of sum of every ROW" << std::endl;
+	printWeightSum();
+
+	printLambda();
+
+	std::cout << "WEIGHTED SUM is sum of each RO of this matrix \n";
+	std::cout << "LAMBDA is WEIGHTED SUM devided by its WEIGHT \n\n";
+
+	printConsisCalc();
 }
 
 std::vector<std::vector<double>> Inconst::getContent() const { return m_content; }
--- Course Work/Inconst.h
+++ Course Work/Inconst.h
@@ -6,9 +6,18 @@ private:
 	std::vector<double> m_weightscopy;
 	std::vector<double> m_lambda;
 	std::vector <double> m_weightsum;
+	double m_lambdaAvg;
+	double m_consisIndex;
+	double m_consisratio;
+	static const double k_randomIndex;
 
 private:
+	double lambdaAvg();
+
 	void printweights();
+	void printWeightSum();
+	void printLambda();
+	void printConsisCalc();
 
 public:
 	Inconst(PairwiseComp& PCTable, Stand& stand);
